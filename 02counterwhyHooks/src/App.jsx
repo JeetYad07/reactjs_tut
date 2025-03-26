ad629e52017cd8cb0cda2a225d3f6bbb10686134
JeetYad07 <jeetb.yadav11@gmail.com>
2025-03-26 11:06:15 +0530

    New commit

--- 02counterwhyHooks/src/App.jsx
+++ 02counterwhyHooks/src/App.jsx
@@ -1,19 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
   // let counter  = 15
   // const addValue = ()  => {
   //   console.log(`Counter value before click`, counter);
-    
+
   //   counter += 1;
   //   console.log(`Counter value after click`, counter, Date.now());
   // }
 
   /**
-   * Till now we have tried to increase the counter value and it is getting update but it is not 
+   * Till now we have tried to increase the counter value and it is getting update but it is not
    * reflecting in dom/screen. why?
    * The problem is that like this way dom will not update,
    * 1: we need to get the dom attributes like h2 tag value by using document.getquerySelector, lot of reference will get
@@ -23,51 +23,45 @@ function App() {
 
   // Then introduction of a function came which is called hooks.
 
-    const  [counter,setCounter] = useState(15)
-    const addValue = ()  => {
+  const [counter, setCounter] = useState(15);
+  const addValue = () => {
     console.log(`Counter value before click`, counter);
-    
-    // counter += 1;
-    if(counter < 20)
-      setCounter(counter + 1)
-    console.log(`Counter value after click`, counter, Date.now());
-      // setCounter(counter + 1);
-      // setCounter(counter + 1);
-      // setCounter(counter + 1);  // All of these are updating only once, this is because of fiber, it takes all of these method as batch
-      // setCounter(counter + 1);  // and we are doing the same task, it will only update once
-      // setCounter(counter + 1);
 
-      // But if interviewer ask, I wanted this to handle multiple update then
-      // setCounter(prevCounter => prevCounter + 1);
-      // setCounter(prevCounter => prevCounter + 1);
-      // setCounter(prevCounter => prevCounter + 1);
-      // setCounter(prevCounter => prevCounter + 1);
-  }
+    // counter += 1;
+    if (counter < 20) setCounter(counter + 1);
+    console.log(`Counter value after click`, counter, Date.now());
+    // setCounter(counter + 1);
+    // setCounter(counter + 1);
+    // setCounter(counter + 1);  // All of these are updating only once, this is because of fiber, it takes all of these method as batch
+    // setCounter(counter + 1);  // and we are doing the same task, it will only update once
+    // setCounter(counter + 1);
+
+    // But if interviewer ask, I wanted this to handle multiple update then
+    // setCounter(prevCounter => prevCounter + 1);
+    // setCounter(prevCounter => prevCounter + 1);
+    // setCounter(prevCounter => prevCounter + 1);
+    // setCounter(prevCounter => prevCounter + 1);
+  };
 
   const removeValue = () => {
     console.log(`Counter value before click`, counter);
-    
+
     // counter += 1;
-    if(counter > 0)
-      setCounter(counter - 1)
-    
+    if (counter > 0) setCounter(counter - 1);
+
     console.log(`Counter value after click`, counter, Date.now());
-  }
+  };
 
   return (
     <>
-     
       <h2>This is counter app to understand, why we need hooks: </h2>
       <h3>Counter value : {counter}</h3>
-      <button onClick={addValue}
-      >Add Value {counter}</button>
+      <button onClick={addValue}>Add Value {counter}</button>
       <br />
-      <button onClick={removeValue}
-      >Remove Value {counter}</button>
+      <button onClick={removeValue}>Remove Value {counter}</button>
       <p>footer value {counter}</p>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ 05passwordGenerator/src/App.jsx
@@ -0,0 +1,104 @@
+import { useCallback, useEffect, useState, useRef } from "react";
+import "./App.css";
+
+function App() {
+  // few variables:
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passwordRef = useRef(null);
+  // need one method
+  // As we need to use the below function on multiple places, that is why we need to optimize it
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*-_+=[]{}~`";
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]);
+
+  const copyPasswordToclipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 99);
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
+  // useEffect
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, charAllowed, passwordGenerator]);
+
+  return (
+    <>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+        <h1 className="text-4xl text-center text-white">Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3 bg-white"
+            placeholder="password"
+            readOnly
+            ref={passwordRef}
+          />
+          <button
+            className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
+            onClick={copyPasswordToclipboard}
+          >
+            copy
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
+            />
+            <label htmlFor="length">length: {length}</label>
+          </div>
+          <div className='"flex items-center gap-x-1"'>
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="numberInputs" className="m-1">
+              Numbers
+            </label>
+          </div>
+          <div className='"flex items-center gap-x-1"'>
+            <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="charInput"
+              onChange={() => {
+                setCharAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="numberInputs" className="m-1">
+              Characters
+            </label>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
